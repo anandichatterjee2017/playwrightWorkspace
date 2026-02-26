7c07df54526b33caf8e8593dd1c20678b5c0a80f
Arunava <chat.bunty@gmail.com>
2026-02-26 16:14:12 +0530

    first commit

---
+++ typeScriptPlaywright/learn_typescript/src/10_tuples.ts
@@ -0,0 +1,12 @@
+/*
+What are Tuples?
+a Tuple is a fixed-length, ordered array where each element can have a different type.
+Unlike regular arrays that allow multiple values of the same type,
+tuples let you define the exact type and number of elements.
+*/
+
+let employee : [string, number] = ["Arunava", 20];
+console.log(employee[0]); //Arunava
+console.log(employee[1]); //20
+
+// Tuple with spread
