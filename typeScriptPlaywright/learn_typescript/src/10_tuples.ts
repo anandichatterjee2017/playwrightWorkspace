b21597788ec5c830034ed65f616d37926b4f1e90
Arunava <chat.bunty@gmail.com>
2026-02-26 21:38:57 +0530

    first commit

--- typeScriptPlaywright/learn_typescript/src/10_tuples.ts
+++ typeScriptPlaywright/learn_typescript/src/10_tuples.ts
@@ -10,3 +10,17 @@ console.log(employee[0]); //Arunava
 console.log(employee[1]); //20
 
 // Tuple with spread
+let data1: [string, number?] = ["Test"];
+
+//Tuple with Spread
+// ... is called the spread operator
+let rgb: [number,number,number,...string[]] = [255,0,0,"Red","Bright","Dark"]
+console.log(rgb[3]);
+
+//Merge two tuples
+let tuple1: [string,number] = ["Rahul",30];
+let tuple2: [boolean,string] = [true,"India"];
+
+let mergeTuple = [...tuple1,...tuple2];
+console.log(mergeTuple);
+console.log(mergeTuple[2]);
