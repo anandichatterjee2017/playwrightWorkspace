f57e60ac35321e70fae16a074d218f0f0446be1e
Arunava <chat.bunty@gmail.com>
2026-02-26 21:58:32 +0530

    first commit

---
+++ typeScriptPlaywright/learn_typescript/src/11_object.ts
@@ -0,0 +1,7 @@
+/*
+What is an Object?
+An Object in Typescript is a collection of key-value pairs(properties), where each key is a string
+and each value can be of any type
+
+Objects can also include methods, which are functions associated with the object.
+*/
