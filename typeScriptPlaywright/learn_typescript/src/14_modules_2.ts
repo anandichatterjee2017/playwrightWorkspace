58bc4560d8a15b3769e78107d250d1506bba5d2f
Arunava <chat.bunty@gmail.com>
2026-02-27 20:26:53 +0530

    code push

--- typeScriptPlaywright/learn_typescript/src/14_modules_2.ts
+++ typeScriptPlaywright/learn_typescript/src/14_modules_2.ts
@@ -1,6 +1,6 @@
 import { EmployeeTeam, adding, something } from "./14_modules_1";
 
-const p1 = new EmployeeTeam("Rahul",30);
+const p1 = new EmployeeTeam("Arunava",30);
 p1.welcome();
 console.log(p1.greet());
 let final = p1.add(10,20);
