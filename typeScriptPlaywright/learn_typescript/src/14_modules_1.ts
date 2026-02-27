babe520962770e559b589f055dc2dfb7f4a0dbdf
Arunava <chat.bunty@gmail.com>
2026-02-27 20:26:21 +0530

    code push

---
+++ typeScriptPlaywright/learn_typescript/src/14_modules_1.ts
@@ -0,0 +1,42 @@
+/*
+What are Modules?
+
+Modules are individual files in Typescript. Anything declared in one module is scoped to 
+that file only unless explicitly exported
+
+We can also export variables and functions
+*/
+export function adding(a: number, b: number): number{
+
+    return a+b;
+}
+
+export const something: number = 123;
+
+export class EmployeeTeam{
+
+    // instance variable
+    emp_name: string;
+    age: number;
+
+    // constructor
+    constructor(empname: string, age: number){
+        this.emp_name = empname;
+        this.age = age;
+    }
+
+    // Function
+    welcome() : void {
+        console.log("Welcome employee")
+    }
+
+    // parameterized function
+    add( a: number, b: number) : number{
+        return a + b;
+    }
+
+    // create function with string
+    greet() : string {
+        return `Hello I am ${this.emp_name}`
+    }
+}
---
+++ typeScriptPlaywright/learn_typescript/src/14_modules_2.ts
@@ -0,0 +1,10 @@
+import { EmployeeTeam, adding, something } from "./14_modules_1";
+
+const p1 = new EmployeeTeam("Rahul",30);
+p1.welcome();
+console.log(p1.greet());
+let final = p1.add(10,20);
+console.log(final)
+
+adding(10,20);
+console.log(something);
