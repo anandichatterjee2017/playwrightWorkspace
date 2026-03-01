4883e47197e9ab6af0963881c451ab5883db3e26
Arunava <chat.bunty@gmail.com>
2026-03-01 14:29:31 +0530

    first commit

---
+++ typeScriptPlaywright/learn_typescript/src/19_exception_handling.ts
@@ -0,0 +1,8 @@
+//Exception Handling
+/*
+try - A block of code where exceptions can occur.
+catch - A block of code to handle the exception
+finally - A block of code that will always execute
+throw - A statement to explicitly throw an exception
+*/
+
