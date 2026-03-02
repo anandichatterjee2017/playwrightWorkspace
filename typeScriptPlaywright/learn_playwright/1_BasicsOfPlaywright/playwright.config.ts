238ccc10cd1284938327f1a23a98395f1b544b6a
Arunava <chat.bunty@gmail.com>
2026-03-02 21:59:28 +0530

    first commit

--- typeScriptPlaywright/learn_playwright/1_BasicsOfPlaywright/playwright.config.ts
+++ typeScriptPlaywright/learn_playwright/1_BasicsOfPlaywright/playwright.config.ts
@@ -42,12 +42,12 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
+    /*{
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
 
-    /*{
+    {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },*/
