8ff96d06f668d966d4a088a114f8b1a415bbeee6
Arunava <chat.bunty@gmail.com>
2026-03-29 00:08:23 +0530

    first commit

--- typeScriptPlaywright/learn_playwright/playwright.config.ts
+++ typeScriptPlaywright/learn_playwright/playwright.config.ts
@@ -29,6 +29,7 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    video: 'off',
     trace: 'on-first-retry',
     browserName: 'chromium',
     channel:'chrome',
