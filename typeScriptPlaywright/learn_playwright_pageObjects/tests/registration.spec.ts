d5921a9052981489fe233634e60a3a8011bd3587
Arunava <chat.bunty@gmail.com>
2026-07-19 21:52:52 +0530

    first commit

--- typeScriptPlaywright/learn_playwright_pageObjects/tests/registration.spec.ts
+++ typeScriptPlaywright/learn_playwright_pageObjects/tests/registration.spec.ts
@@ -1,6 +1,7 @@
 //import {test, expect} from '@playwright/test';
 //import { Register } from '../pages/RegisterClass';
 //import { HomePage } from '../pages/HomePage';
+import { readCSV } from '../utils/readCSV';
 import { test, expect } from '../utils/test_base';
 
 test.describe('User Registration Test', () => {
@@ -23,5 +24,16 @@ test.describe('User Registration Test', () => {
         const password = 'Test@1234';
         const mobile = '2025550123';
         await pages.Register.user_register(email, password, mobile);
+    });
+
+    const testData = readCSV('data/testdata.csv');
+    test('should register a new user - Parameterized', async ({pages}) => {
+        for(const data of testData) {
+            let headerText = await pages.Register.getHeaderText();
+            expect(pages.page).toHaveURL(/.*doRegistration/);
+            expect(headerText).toContain('Join the family');
+            const email = ''+ Math.floor(Math.random() * 10000) + '@example.com';
+            await pages.Register.user_register(email, data.password, data.mobile);
+        }
     });
 });
